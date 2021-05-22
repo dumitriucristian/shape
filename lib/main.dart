c8cfd41693f83decd7a38cc70a53b7567b54f9ff
cdumitriu <cdumitriu@pentalog.com>
2021-05-22 13:12:33 +0300

    fix cube and triangle algorithm

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(
+    MaterialApp(
        title: "Shape",
        home: Shape()
     ),
@@ -27,13 +28,9 @@ class _ShapeState extends State<Shape> {
   //17:32: Error: Method not found: '_ShapeState'.
   @override
   Widget build(BuildContext context) {
-    //de ce aici e ok
-    Cube _cube = new Cube(number:4);
-    //print(_cube.number);
-   // print(_cube.isValidShape());
-    Triangle _triangle = new Triangle(number:4);
-   // print(_triangle.number);
-   // print(_triangle.isValidShape());
+    //de ce aici e ok?
+    //Cube _cube = new Cube(number:4);
+
     TextEditingController _controller = TextEditingController();
 
     return Scaffold(
@@ -43,23 +40,80 @@ class _ShapeState extends State<Shape> {
       ),
       body: Column(
         children: [
-          Text('some text'),
+          Text('Pleas input some number to see if it is square or triangular'),
           TextFormField(
             controller: _controller,
             ),
           ],
       ),
       floatingActionButton: FloatingActionButton(onPressed: (){
-        validateInput(_controller.text);
-      }),
+           String validate = validateInput(_controller.text);
+           if( null != validate) {
+             showDialog(context: context,
+                 builder: (BuildContext context) => AlertDialog(
+                  title: Text("$validate"),
+                  actions: [
+                     TextButton(
+                        onPressed: () {
+                          Navigator.pop(context, 'Cancel');
+                          _controller.clear();
+                        },
+                        child: Text('Try again'),
+                      ),
+                    ]
+               ),
+             );
+           }else{
+
+             int _number =  num.parse(_controller.text);
+              Cube _cube = Cube(number: _number);
+               Triangle _triangle = Triangle(number: _number);
+              bool _isCube = _cube.isValidShape();
+              bool _isTriangle = _triangle.isValidShape();
+              print(_isTriangle);
+              String _message = checkShape(_isCube, _isTriangle, _number);
+
+              showDialog(context: context,
+               builder: (BuildContext context) => AlertDialog(
+                   title: Text("$_message"),
+                   actions: [
+                     TextButton(
+                       onPressed: () {
+                         Navigator.pop(context, 'Cancel');
+                         _controller.clear();
+                       },
+                       child: Text('Try again'),
+                     ),
+                   ]
+               ),
+             );
+           };
+        },
+      ),
     );
   }
 }
 
+String checkShape(bool cube, bool triangle, number)
+{
+  if((cube == true) && (triangle == true)) {
+    return "Number $number is triangle and cube";
+  }else if( (cube == false) && (triangle == true) ){
+    return "Number $number is triangle";
+  }else if( (cube == true) && (triangle == false)){
+    return "Number $number is cube";
+  }else if( (cube == false) && (triangle == false)){
+    return "Number $number is neither cube or triangle";
+  }else{
+    return "Nasty logic with nasty results happened";
+  }
+}
+
+
 String validateInput(String value) {
   final number = num.tryParse(value);
   if(value.isEmpty || number == null) {
-    print( 'Please use only numbers');
+    return 'Please use only numbers';
   }
   return null;
 }
@@ -72,14 +126,14 @@ abstract class GeometricShape{
 class Cube implements GeometricShape {
   int number;
   int _counter = 0;
-
+  int _product = 0;
   Cube({this.number});
 
   bool isValidShape() {
-    while(_counter < number) {
-      int product = _counter * _counter;
+    while(_product < number) {
+      _product = _counter * _counter * _counter;
       _counter++;
-      if(product == number) {
+      if(_product == number) {
         return true;
       }
     }
@@ -91,12 +145,14 @@ class Triangle implements GeometricShape {
   int number;
   int _counter = 0;
   int _sum = 0;
+
   Triangle({this.number});
 
   bool isValidShape(){
-    while(_counter < number) {
-      int _sum = _counter + 1;
-      _counter++;
+    while(_sum < number) {
+        this._sum = _sum + (_counter +1);
+        ++_counter;
+        print(_sum);
       if(_sum == number) {
         return true;
       }
@@ -104,4 +160,3 @@ class Triangle implements GeometricShape {
     return false;
   }
 }
-
